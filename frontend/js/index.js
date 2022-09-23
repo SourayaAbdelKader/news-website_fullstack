67906982bafd8152a6e7d6abae0cc0100f4435b2
Sourayaak <sourayaabdelkader.gmail.com>
2022-09-23 18:21:09 +0300

    styling the art page

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -4,10 +4,14 @@ $.getJSON("http://localhost/news-assignment/backend/art-articles.php", function 
     const art_title = document.getElementById("art-title");
     const art_author = document.getElementById("art-author");
     const art_article = document.getElementById("art-article");
-    const art_index =0;
+    const art_image = document.getElementById("art-image");
+    const art_brief = document.getElementById("brief");
+    art_image.src = data[0].images;
+    art_brief.innerHTML = data[0].brief
     art_title.innerHTML = data[0].title;
     art_author.innerHTML = data[0].author;
     art_article.innerHTML = data[0].article;
     console.log(data[0].title)
     console.log(typeof(data[0].title))
     });
+
